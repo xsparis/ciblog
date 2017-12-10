eac0d521deaee1978927edb9d007e98b8dc36039
xsparis <xsparis@outlook.com>
2017-12-10 17:34:19 +0100

    create file User_model

---
+++ application/models/User_model.php
@@ -0,0 +1,22 @@
+<?php
+class User_model extends CI_Model {
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function get_users($id=false){
+
+    }
+
+    public function create_user(){
+
+    }
+
+    public function delete_user($id){
+
+    }
+
+    public function edit_user($id){
+        
+    }
+}
